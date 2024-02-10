6c9b5ebbf3b56b1c05958458229c5a8878dc1966
Venkatesh Prasad <venkatesh.prasad@adroit-vantage.com>
2024-02-10 15:26:38 +0530

    prisma course completed

---
+++ notes/installation.js
@@ -0,0 +1,46 @@
+// 1. npm init -y
+// 2. npm install typescript ts-node @types/node --save-dev
+// 3. npx tsc --init
+// 4. npm install prisma--save - dev
+// 5. npx prisma init--datasource - provider postgresql
+// 6. Model your data in the Prisma schema
+
+// At this point, you have a Prisma schema but no database yet.
+// Run the following command in your terminal to create the SQLite database
+// and the User and Post tables represented by your models:
+
+// npx prisma migrate dev --name init
+// npx prisma studio : open db locally
+
+// What all to learn in postgressql
+//  1. Setting up PostgreSQL with Docker
+//  2. Setting up Prisma
+//  3. Data Models
+//  4. Introspection and Migrations
+//  5. Defining Data Models
+//  6. Runing Migrations
+//  7. Attributes: Modify the behavior of fields and model
+//  8. Prisma Studio
+//  9. Enums
+//  10. Relationships
+//  11. One-To-Many Relationships
+//  12. Many-To-Many Relationships
+//  13. Setting up Prisma Client
+//  14. Fetch data with Find functions
+//  15. Seed the data base
+//  16. Filters: How we can narrow down result of a query
+//  17. Operators in Filters
+//  18. Relation Filters
+//  19. Nested queries
+//  20. Selecting Specific Fields
+//  21. Aggregation Functions
+//  22. Group By
+//  23. Sort the Result of a Query
+//  24. Pagination
+//  25. Offset Pagination
+//  26. Cursor Pagination
+//  27. Insert data to data base
+//  28. Cascading Delete and Update
+//  29. Take Inserting data from client
+//  30. Update,Upsert,Delete
+//  31. Transactions
